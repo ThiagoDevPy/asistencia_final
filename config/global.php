17e421913f43122d4dceb9548352f2586a85213e
ThiagoDevPy <sebastiansaldivar092@gmail.com>
2024-10-14 20:22:37 -0300

    new connnetion

--- config/global.php
+++ config/global.php
@@ -1,16 +1,16 @@
 <?php
 //IP del servidor de la base de datos
-define("DB_HOST", "fra1.clusters.zeabur.com");
+define("DB_HOST", "junction.proxy.rlwy.net");
 
 //nombre de la base de datos
-define("DB_NAME", "asistencia");
+define("DB_NAME", "railway");
 
 //usuario de la base de datos
 define("DB_USERNAME", "root");
 
 
 // contrasena de la base de datos
-define("DB_PASSWORD", "F1452gwrd7t6MYJXEBAZz0pu98vWN3yj");
+define("DB_PASSWORD", "jTegmeDUEAScouxevhaXXazZTThvNptv");
 
 //codificacion de caracteres
 define("DB_ENCODE", "utf8");
@@ -21,6 +21,6 @@ define("PRO_NOMBRE", "CompartiendoCodigo");
 //Zona horaria
 define("ZONA_HORARIA", "America/Asuncion");
 
-define("PORT", "30089");
+define("PORT", "57690");
 
 ?>
