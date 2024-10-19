84a97f06ae19280109e9a7b90ff3c2ed0c159baa
ThiagoDevPy <sebastiansaldivar092@gmail.com>
2024-10-19 08:47:19 -0300

    new connetion

--- config/global.php
+++ config/global.php
@@ -1,16 +1,16 @@
 <?php
 //IP del servidor de la base de datos
-define("DB_HOST", "autorack.proxy.rlwy.net");
+define("DB_HOST", "fra1.clusters.zeabur.com");
 
 //nombre de la base de datos
-define("DB_NAME", "railway");
+define("DB_NAME", "zeabur");
 
 //usuario de la base de datos
 define("DB_USERNAME", "root");
 
 
 // contrasena de la base de datos
-define("DB_PASSWORD", "SttfjyiqKhWHXUzGiGbokHowQCWFWpOQ");
+define("DB_PASSWORD", "3Hax6NeFyIRVC8Z712S5Pkmbg9Mcu40i");
 
 //codificacion de caracteres
 define("DB_ENCODE", "utf8");
@@ -21,6 +21,6 @@ define("PRO_NOMBRE", "CompartiendoCodigo");
 //Zona horaria
 define("ZONA_HORARIA", "America/Asuncion");
 
-define("PORT", "31433");
+define("PORT", "30467");
 
 ?>
