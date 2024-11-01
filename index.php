e8298ba9304c1e55640370f5ccd690d4eda7292b
ThiagoDevPy <sebastiansaldivar092@gmail.com>
2024-11-01 13:07:36 -0300

    asdaadasdadad

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php 
-header('location: vistas/escritorio.php');
+
+// Manejar otras rutas
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = trim($uri, '/');
+
+if ($uri === 'login') {
+    include 'vistas/login.php';
+} elseif ($uri === 'escritorio') {
+    include 'vistas/escritorio.php';
+} else {
+    // Maneja el error o redirige a la vista predeterminada
+    include 'vistas/404.php'; // Asegúrate de tener una vista 404
+}
