422419d048fdf1f04893c61350521255ee76f5e3
ThiagoDevPy <sebastiansaldivar092@gmail.com>
2024-11-01 01:04:30 -0300

    new url

--- vistas/updateqr.php
+++ vistas/updateqr.php
@@ -29,7 +29,7 @@ if (isset($_GET['id'])) {
         $stmt->bind_param("s", $new_id);
         $stmt->execute();
 
-        $new_qr_code_data = "https://asis.zeabur.app/controlador/guardardatos.php?id=" . $new_id;  // tiene que ser igual a la url del la pagina
+        $new_qr_code_data = "https://asis.fra1.zeabur.app/controlador/guardardatos.php?id=" . $new_id;  // tiene que ser igual a la url del la pagina
         QRcode::png($new_qr_code_data, 'qrcodes/new_qr.png', QR_ECLEVEL_L, 10);
 
         echo json_encode(['new_qr' => "qrcodes/new_qr.png", 'new_id' => $new_id]);
